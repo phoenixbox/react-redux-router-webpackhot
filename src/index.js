3d0ceb5e71d4797f594827651e4086094263d15a
Shane Rogers <shane@repllabs.com>
2016-01-27 16:48:17 +0000

    Redux skeleton with bootstap

--- src/index.js
+++ src/index.js
@@ -1,19 +1,54 @@
-import React from 'react';
-import App from './App';
-import Home from './Home';
-import About from './About';
+import { createDevTools } from 'redux-devtools'
+import LogMonitor from 'redux-devtools-log-monitor'
+import DockMonitor from 'redux-devtools-dock-monitor'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { applyMiddleware, compose, createStore, combineReducers } from 'redux'
+import { Provider } from 'react-redux'
+import { Router, Route, IndexRoute } from 'react-router'
+import createHistory from 'history/lib/createHashHistory'
+import { syncHistory, routeReducer } from 'react-router-redux'
 
-import { render } from 'react-dom'
-import { Router, Route, IndexRoute, Redirect } from 'react-router';
+// Redux
+import * as reducers from './reducers'
+// Components
+import { App, Home, Foo, Bar } from './components'
+// Styles
+require('./less/bootstrap/bootstrap.less');
 
-import history from './history'
+const history = createHistory()
+const middleware = syncHistory(history)
+const reducer = combineReducers({
+  ...reducers,
+  routing: routeReducer
+})
 
-render(
-  <Router history={history}>
-    <Route path="/" component={App}>
-      <Route name="about" component={About} />
-      <IndexRoute name="home" component={Home} />
-    </Route>
-  </Router>,
+const DevTools = createDevTools(
+  <DockMonitor toggleVisibilityKey="ctrl-h"
+               changePositionKey="ctrl-q">
+    <LogMonitor theme="tomorrow" />
+  </DockMonitor>
+)
+
+const finalCreateStore = compose(
+  applyMiddleware(middleware),
+  DevTools.instrument()
+)(createStore)
+const store = finalCreateStore(reducer)
+middleware.listenForReplays(store)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <div>
+      <Router history={history}>
+        <Route path="/" component={App}>
+          <IndexRoute component={Home}/>
+          <Route path="foo" component={Foo}/>
+          <Route path="bar" component={Bar}/>
+        </Route>
+      </Router>
+      <DevTools />
+    </div>
+  </Provider>,
   document.getElementById('root')
-);
+)
